0b76c53717b6372e6d88b84774aff69fcbd79f29
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2019-02-12 23:00:43 +0900

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
 	env: { browser: true },
-	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
+	extends: [
+		'plugin:@wordpress/eslint-plugin/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
 };
